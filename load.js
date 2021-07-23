e67d9051d91039cbbaa319a36a23ccbba3a53ea8
retr0_ <82467575+retroXyz@users.noreply.github.com>
2021-07-23 16:01:58 +0300

    Update load.js

--- load.js
+++ load.js
@@ -1,2 +1,15 @@
+// preloader.onload
+
 const preloader = document.getElementById("preloader");
 
+// EventListenersAdding.inload
+
+window.addEventListener('load', ()=>{
+
+  preloader.style.cursor = 'auto';
+  
+  setTimeout(()=>{
+    
+    preloader.classList.add('remove');
+
+});
